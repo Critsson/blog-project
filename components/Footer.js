ba31fe0decb02f3169fe17e817651b8f98a52770
Chris <chris.gao77@gmail.com>
2022-07-02 01:07:55 -0400

    Update

--- components/Footer.js
+++ components/Footer.js
@@ -6,61 +6,65 @@ import mailIcon from "../public/images/mail_icon.svg"
 import phoneIcon from "../public/images/phone_icon.svg"
 import instaLogo from "../public/images/insta_logo.svg"
 import facebookLogo from "../public/images/facebook_logo.svg"
+import logoFooter from "../public/images/glorious_diaries_footer.png"
 
 
 export default function Footer() {
     return (
         <div className={styles.container}>
+            <div className={styles.logo_footer}>
+                <Image src={logoFooter} alt="Glorious Diaries Footer Logo" width={470} height={100} />
+            </div>
             <div className={styles.navigate_container}>
                 <h1>navigate.</h1>
                 <div className={styles.links_container}>
                     <div>
                         <Link href="/">
-                            <h2>Home ></h2>
+                            <h2>Home &gt;</h2>
                         </Link>
                         <Link href="/about">
-                            <h2>About ></h2>
+                            <h2>About &gt;</h2>
                         </Link>
                         <Link href="/team">
-                            <h2>Our Team ></h2>
+                            <h2>Our Team &gt;</h2>
                         </Link>
                         <Link href="/shop">
-                            <h2>Shop ></h2>
+                            <h2>Shop &gt;</h2>
                         </Link>
                         <Link href="/commissions">
-                            <h2>Commissions ></h2>
+                            <h2>Commissions &gt;</h2>
                         </Link>
                     </div>
                     <div>
-                        <h2>Journey to 1000 Trails ></h2>
-                        <h2>Medlife ></h2>
-                        <h2>To read/watch ></h2>
+                        <h2>Journey to 1000 Trails &gt;</h2>
+                        <h2>Medlife &gt;</h2>
+                        <h2>To read/watch &gt;</h2>
                     </div>
                 </div>
             </div>
             <div className={styles.ama_container}>
                 <h1>ask me anything.</h1>
                 <div className={styles.ama_info}>
-                    <Image src={mailIcon} rel="Mail Icon" />
+                    <Image src={mailIcon} alt="Mail Icon" />
                     <h3>QandA@gloriousdiaries.com</h3>
                 </div>
                 <div className={styles.ama_info}>
-                    <Image src={phoneIcon} rel="Phone Icon" />
+                    <Image src={phoneIcon} alt="Phone Icon" />
                     <h3>+1 (###) - ### - ####</h3>
                 </div>
             </div>
             <div>
-                <h1>let's connect.</h1>
+                <h1>let&apos;s connect.</h1>
                 <div className={styles.connect_icons}>
                     <div className={styles.connect_icon}>
-                        <Link href="https://www.instagram.com/glostar_ma/">
-                            <Image src={instaLogo} rel="Instagram Logo" />
-                        </Link>
+                        <a target="_blank" href="https://www.instagram.com/glostar_ma/" rel="noopener noreferrer">
+                            <Image src={instaLogo} alt="Instagram Logo" />
+                        </a>
                     </div>
                     <div className={styles.connect_icon}>
-                        <Link href="https://www.facebook.com/xixi.ma.14">
-                            <Image src={facebookLogo} rel="Facebook Logo" />
-                        </Link>
+                        <a target="_blank" href="https://www.facebook.com/xixi.ma.14" rel="noopener noreferrer">
+                            <Image src={facebookLogo} alt="Facebook Logo" />
+                        </a>
                     </div>
                 </div>
             </div>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,23 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import ellipse1 from "../public/images/ellipse_1.png"
+import ellipse2 from "../public/images/ellipse_2.png"
+import welcomeImage from "../public/images/welcome.svg"
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
-    <div className={styles.main}>
-
+    <div className={styles.container}>
+      <div className={styles.banner}>
+        <div className={styles.center_banner}>
+          <Image src={welcomeImage} alt="Welcome to the Blog" width={350} height={100} />
+          <p>Whatever is your reason for stopping by, I hope you find what you&apos;re looking for and enjoy your stay</p>
+          <div>
+            <Image src={ellipse1} alt="Creative Works" width={200} height={200} />
+            <Image src={ellipse2} alt="Entertainment" width={200} height={200} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
