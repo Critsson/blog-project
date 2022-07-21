56c7336fee742ec6d93318983a995ca5877c2c72
Chris <chris.gao77@gmail.com>
2022-07-20 22:16:15 -0400

    Desktop About page finished

--- pages/about.js
+++ pages/about.js
@@ -2,8 +2,17 @@ import styles from "../styles/About.module.css"
 import aboutImage from "../public/images/about.png"
 import Image from "next/image"
 import headshot from "../public/images/headshot.svg"
+import Link from "next/link"
+import ellipse5 from "../public/images/ellipse_5.png"
+import ellipse6 from "../public/images/ellipse_6.svg"
+import ellipse7 from "../public/images/ellipse_7.svg"
+import React from "react"
 
 export default function about() {
+
+    const [herePressed, setHerePressed] = React.useState(false)
+
+
     return (
         <div>
             <div className={styles.header}>
@@ -13,7 +22,7 @@ export default function about() {
             </div>
             <div className={styles.first_container}>
                 <div className={styles.headshot}>
-                    <Image src={headshot} alt="Picture of Gloria" width="600%" height="460%"/>
+                    <Image src={headshot} alt="Picture of Gloria" width="600%" height="460%" />
                 </div>
                 <div className={styles.first_paragraph}>
                     <div className={styles.first_line}>
@@ -27,6 +36,23 @@ export default function about() {
             <p className={styles.second_paragraph}>fondly on my life&apos;s chronicles, and be transparent about the humanity behind some of society&apos;s greatest heroes</p>
             <p className={styles.third_paragraph}>Some of the topics I write about include my hobbies:</p>
             <div className={styles.ellipse_container}>
+                <Link href="/blog/fitnessandhealth">
+                    <div className={styles.ellipse_5}>
+                        <Image src={ellipse5} className={styles.ellipse5} width="500" height="500" />
+                    </div>
+                </Link>
+                <Link href="/blog/creativeworks">
+                    <div className={styles.ellipse_6}>
+                        <Image src={ellipse6} className={styles.ellipse6} width="500" height="500" />
+                    </div>
+                </Link>
+                <Link href="/blog/entertainment">
+                    <div className={styles.ellipse_7}>
+                        <Image src={ellipse7} className={styles.ellipse7} width="500" height="500" />
+                    </div>
+                </Link>
+            </div>
+            <div className={styles.banner_container}>
                 <div className={styles.small_banner_5}>
                     Fitness
                 </div>
@@ -36,13 +62,26 @@ export default function about() {
                 <div className={styles.small_banner_7}>
                     Anime/Manga
                 </div>
-                <div className={styles.ellipse_5}>
-                </div>
-                <div className={styles.ellipse_6}>
-                </div>
-                <div className={styles.ellipse_7}>
-                </div>
             </div>
+            <p className={styles.fourth_paragraph}>Additionally, I&apos;ll sometimes talk about what it&apos;s like to be a medical student, my journey, and any tips/advice I may have!</p>
+            <div className={styles.last_container}>
+                <p className={styles.fifth_paragraph}>If you have any specific topics or questions you&apos;d like me to answer in a post, make your submission</p>
+                <p onClick={() => setHerePressed(true)} className={styles.here}>here!</p>
+            </div>
+            {herePressed &&
+                <div className={styles.dear_container}>
+                <h2>“Dear Glostar...”</h2>
+                <div>
+                  <input className={styles.name_text} type="text" placeholder='Name...' />
+                  <div className={styles.checkbox_container}>
+                    <input className={styles.checkbox} type="checkbox" id="check" />
+                    <label className={styles.checkbox_label} htmlFor="check">Check to remain anonymous</label>
+                  </div>
+                  <textarea className={styles.text_area} placeholder='Message...' />
+                  <button className={styles.text_area_button}>Submit</button>
+                </div>
+              </div>
+            }
         </div>
     )
 }
