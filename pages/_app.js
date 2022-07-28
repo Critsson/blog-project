037ff5f9a0880413e3ca521f4a13518a4086170d
Chris <chris.gao77@gmail.com>
2022-07-28 17:02:03 -0400

    Fixed cms

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,6 @@ import '../styles/globals.css'
 import Header from "../components/Header"
 import Footer from "../components/Footer"
 import Head from "next/head"
-import Script from "next/script"
 
 function MyApp({ Component, pageProps }) {
 
@@ -14,12 +13,10 @@ function MyApp({ Component, pageProps }) {
     <>
       <Head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <Script src="https://identity.netlify.com/v1/netlify-identity-widget.js" />
       </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
-      <Script src="../netlifycms.js" />
     </>
   )
 }
